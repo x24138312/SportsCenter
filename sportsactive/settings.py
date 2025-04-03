a22f6de624c9f5346569c7f46627099a399ac355
x24138312 <x24138312@student.ncirl.ie>
2025-04-03 11:24:25 +0530

    Update settings.py

--- sportsactive/settings.py
+++ sportsactive/settings.py
@@ -125,18 +125,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS=[BASE_DIR/'static']
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
 STATIC_URL = "static/"
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "staticfiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
