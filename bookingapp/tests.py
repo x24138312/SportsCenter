f17d51db9b789c807640256a51a8f52f179bb6cd
x24138312 <x24138312@student.ncirl.ie>
2025-04-03 11:28:00 +0530

    Update tests.py
    Added Test Cases

--- bookingapp/tests.py
+++ bookingapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class MyViewTestCase(TestCase):
+    def test_home_view(self):
+        """Test if login page returns 200 status"""
+        response = self.client.get(reverse('login_page'))
+        self.assertEqual(response.status_code, 200)
