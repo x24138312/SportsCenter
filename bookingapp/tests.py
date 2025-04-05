75e5e2f4fee184c80b0a8ed8e3b533641d8f501e
x24138312 <x24138312@student.ncirl.ie>
2025-04-05 10:49:41 +0100

    Update tests.py

--- bookingapp/tests.py
+++ bookingapp/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class MyViewTestCase(TestCase):
+    def test_home_view(self):
+        """Test if login page returns 200 status"""
+        response = self.client.get(reverse('login_page'))
+        self.assertEqual(response.status_code, 200)
+
+class MyViewTestCase(TestCase):
+    def test_home_view(self):
+        """Test if  page returns 200 status"""
+        response = self.client.get(reverse('about_us'))
+        self.assertEqual(response.status_code, 200)
